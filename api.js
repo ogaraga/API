af048105f15464a386516eeceaa028f50b584f6e
Ogaraga <raymond.ogaraga@gmail.com>
2023-09-28 18:13:44 +0100

    review

--- api.js
+++ api.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const app = express();
 const mongoose = require('mongoose');
 const mostIgboNames = require('./routes/namesRoute');
+const bodyParser = require('body-parser');
 
 //VARIABLE DECLARATION 
 const goodConnection = 'Database connected!';
@@ -12,10 +13,12 @@ const badConnection = 'Database disconnected!';
 //connecting to database
 mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => console.log(`${goodConnection}`)).catch(() => console.log(`${badConnection}`));
 
-
+ 
 //MIDDELWARE
 app.use(express.json());
-app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+app.use(express.urlencoded({ extended: false, limit: '50mb' }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false, limit: '50mb' }));
 app.use(cors());
 app.use('/', mostIgboNames);   
  
--- routes/namesRoute.js
+++ routes/namesRoute.js
@@ -2,22 +2,22 @@ const router = require('express').Router();
 const myNames = require('../igboNames/names')
 
 //post logic here
-router.post('/api/names', async (req, res, next) => {
-  const { name } = req.body;
-  try {
-    const foundName = await myNames.find(aham=>(aham.name === name) || (aham.name.toLowerCase() === name) || (aham.name.toUpperCase() === name));
-    if (name === foundName.name.toLowerCase() || name === foundName.name.toUpperCase() || name === foundName.name)
-      res.status(200).json(
-  {
-    Message:' name found',
-    NameInfo: foundName
-  }
-);
-  } catch (error) {
-    res.json(error.message)
-  }
-  next();
-});
+// router.post('/api/names', async (req, res, next) => {
+//   const { name } = req.body;
+//   try {
+//     const foundName = await myNames.find(aham=>(aham.name === name) || (aham.name.toLowerCase() === name) || (aham.name.toUpperCase() === name));
+//     if (name === foundName.name.toLowerCase() || name === foundName.name.toUpperCase() || name === foundName.name)
+//       res.status(200).json(
+//   {
+//     Message:' name found',
+//     NameInfo: foundName
+//   }
+// );
+//   } catch (error) {
+//     res.json(error.message)
+//   }
+//   next();
+// });
 
 //get logic
 router.get('/api/names', async (req, res, next) => {
